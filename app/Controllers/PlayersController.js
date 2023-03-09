14b65727b04e73af379aa32db733932a31778a75
Alexmquan <alexmquan93@gmail.com>
2023-03-09 16:10:49 -0700

    created draw

--- app/Controllers/PlayersController.js
+++ app/Controllers/PlayersController.js
@@ -2,12 +2,24 @@ import { appState } from "../AppState.js";
 import { Player } from "../Models/Player.js";
 import { playersService } from "../Services/PlayersService.js";
 import { getFormData } from "../Utils/FormHandler.js"
+import { setHTML } from "../Utils/Writer.js";
 
+function _drawPlayer() {
+  const players = appState.players
+  let template = ''
+  players.forEach(p => template += p.PlayerTemplate)
+  setHTML('players', template)
+}
 
+function _drawActive() {
+  let activePlayer = appState.activePlayer
+  setHTML('active')
+}
 
 export class PlayersController {
   constructor() {
-
+    _drawPlayer()
+    appState.on('activePlayer', _drawActive)
 
   }
 
@@ -18,5 +30,9 @@ export class PlayersController {
     console.log(form)
     let formData = getFormData(form)
     playersService.createPlayer(formData)
+  }
+
+  setActivePlayer(player) {
+    playersService.setActivePlayer(player)
   }
 }
